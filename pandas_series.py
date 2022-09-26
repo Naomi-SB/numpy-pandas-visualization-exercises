ffe1339b2b7351df5e0b16c5ab34584edc94c729
Naomi Sweatt <sweatt.b.naomi@gmail.com>
2022-09-26 09:00:44 -0500

    exercises 1 and 2 complete ready for check

--- pandas_series.py
+++ pandas_series.py
@@ -53,16 +53,26 @@ fruit_series.str.count('a')
 
 # 3. Output the number of vowels in each and every string value.
 fruit_series.str.count('[aeiou]')
+
 # 4. Write the code to get the longest string value from fruits.
+    #to identify the length of the longest string:
+fruit_series.str.len().max()
+    # to output the name of the fruit with the longest string value:
+fruit_series[fruit_series.str.len().idmax()]
 
 # 5. Write the code to get the string values with 5 or more letters in the name.
+fruit_series[fruit_series.str.len() >= 5]
 
 #6. Find the fruit(s) containing the letter "o" two or more times.
 fruit_series.str.count('o') >= 2 
 
 #7. Write the code to get only the string values containing the substring "berry".
 fruit_series[fruit_series.str.contains('berry')]
+
 #8. Write the code to get only the string values containing the substring "apple".
 fruit_series[fruit_series.str.contains('apple')]
-#9. Which string value contains the most vowels?Capitalize all the string values in fruits.
+
+#9. Which string value contains the most vowels?
+fruit_series.str.count('[aeiou]').max()
+
 
